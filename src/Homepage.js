e423af3fa3db0d252c0fccc164197c9c7582aef1
nickryan07 <nickryan07@sbcglobal.net>
2019-01-27 14:59:33 -0600

    removed comment

--- src/Homepage.js
+++ src/Homepage.js
@@ -44,7 +44,6 @@ class Homepage extends Component {
                     <Tabs>
                         <Tab heading="Tab 1">
                             <Container>
-                                {/*<Header/>*/}
                                 <Content padder>
                                     <Form>
                                         <Item floatingLabel>
